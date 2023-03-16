13a4b3456811904fab182316b133d2a359c4512c
ErikGaniev <91753105+ErikGaniev@users.noreply.github.com>
2023-03-16 07:32:34 +0500

    Update lab_1_euler.cpp

--- lab_1_euler.cpp
+++ lab_1_euler.cpp
@@ -1,32 +1,32 @@
 
-
 #include <iostream>
 #include <cmath>   
 //#include <easy_plot.hpp>
 using namespace std;
 double y_0 = -0.733026, h = 0.1;
+double xi[11], yi[11], yr3[11];
 
-double y (double x)// заголовок функции
+double y(double x)// заголовок функции
 {
-  return x * log (log (x));
+    return x * log(log(abs(x)));
 }
 
-double f_d (double z, double q)// заголовок функции
+double f_d(double z, double q)// заголовок функции
 {
-  return q / z + 1 / log (z) ;
+    return q / z + 1 / log(abs(z));
 }
 
-double euler (double x, double y)
-{  
-  return y = y + h * f_d (x, y) ;
+double euler(double x, double y)
+{
+    return y = y + h * f_d(x, y);
 }
 
-double dy(double k_1, double k_2, double k_3, double k_4 )
+double dy(double k_1, double k_2, double k_3, double k_4)
 {
     return h * (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6;
 }
 
-double *method_euler()
+double* method_euler()
 {
     double f[11];
     f[0] = euler(2, y_0);// запуск функции нахождения 
@@ -35,9 +35,10 @@ double *method_euler()
     for (int i = 1; i < 11; i++)
     {
         f[i] = euler(2 + i * h, h * f[i - 1]);// запуск функции нахождения 
-        cout << "x= " << 2 + i * h << " f= " << f[i] << " " << y(2 + i * h) << endl; // печать значения 
-    }   
-    return f;
+        cout << "x= " << 2 + i * h << " f= " << f[i] << " y= " << y(2 + i * h) << " ut-up= " << abs(f[i] - y(2 + i * h)) << endl; // печать значения 
+    }
+
+     return f;
 }
 
 double* method_rundi()
@@ -47,37 +48,65 @@ double* method_rundi()
     double k3 = f_d(2 + h / 2, y_0 + h * k2 / 2);
     double k4 = f_d(2 + h, y_0 + h * k3);
     double yrk[11];
-    yrk[0] = y_0 + dy(k1, k2, k3, k4);// запуск функции нахождения 
-    cout << "x= " << 2 + h << " " << yrk[0] << endl; // печать начального значения 
+    yrk[0] = y_0;// запуск функции нахождения 
+   
+    cout << "x= " << 2  << " y=f= " << yrk[0] << endl; // печать начального значения 
 
-    for (int i = 1; i < 10; i++)
+    for (int i = 1; i < 11; i++)
     {
         k1 = f_d(2 + i * h, yrk[i - 1]);
         k2 = f_d(2 + i * h + h / 2, yrk[i - 1] + h * k1 / 2);
         k3 = f_d(2 + i * h + h / 2, yrk[i - 1] + h * k2 / 2);
         k4 = f_d(2 + i * h + h, yrk[i - 1] + h * k3);
         yrk[i] = yrk[i - 1] + dy(k1, k2, k3, k4);// запуск функции нахождения 
-        cout << "x= " << 2 + i * h + h << " " << yrk[i] << endl; // печать начального значения 
+        cout << "x= " << 2 + i * h  << " y= " << y(2 + i * h) << " f= " << yrk[i] << " ut-up= " << abs(yrk[i] - y(2 + i * h + h)) << endl; // печать начального значения 
 
     }
-    return yrk;
+
+     return yrk;
 }
 
 
 int main()
 {
-   
+    for (int i = 0; i < 11; i++)
+    {
+        xi[i] = 2 + i * h;
+        yi[i] = y(xi[i]);
+        cout <<xi[i]<< " " << yi[i] << endl;
+    }
+
     // 1 решение методом Эйлера
-    cout << "met1:" << endl;
-    double  *otv1= method_euler();
+    cout << "met1 :" << endl;
+
+    double* otv1 = method_euler();
     //cout << otv1[2]<<" ";
     //cout << otv1[2];
     // 2 решение методом Рунди-Кута 4 порядка
-   
+
     cout << "met2:" << endl;
     double* otv2 = method_rundi();
-    
-    cout << "met1:" << endl;
 
-     return 0;
+
+    cout << "met3:" << endl;
+
+
+    yr3[0] = yi[0];
+     // метод рунди кута третьего порядка
+    double k13 = f_d(xi[0] , yr3[0] );
+    double k23 = f_d(xi[0] + h / 2, yr3[0] + h * k13 / 2);
+    double k33 = f_d(xi[0] + h, yr3[0] - h * k13 +2*h*k23);
+
+
+     for (int i = 1; i < 11; i++)
+      {
+         yr3[i] =yr3[i-1]+ h * (k13 + 4 * k23 + k33) / 6;
+         k13 = f_d(xi[i], yr3[i]);
+         k23 = f_d(xi[i] + h / 2, yr3[i] + h * k13 / 2);
+         k33 = f_d(xi[i] + h, yr3[i] - h * k13 + 2 * h * k23);
+         cout<<"x="<<xi[i] << " y=" << yr3[i] << " ut-up= " << abs(yr3[i] - y(2 + i * h + h)) << endl; 
+     }
+
+
+    return 0;
 }
